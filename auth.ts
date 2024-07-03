42e8a3d3bb13a8c304b34de3dc08bdd8aaa09f72
if <if.mung@gmail.com>
2024-07-03 10:48:36 +0800

    feat: update auth config

--- auth.ts
+++ auth.ts
@@ -1,6 +1,19 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
+import "next-auth/jwt"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
 })
+
+declare module "next-auth" {
+  interface Session {
+    accessToken?: string
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    accessToken?: string
+  }
+}
